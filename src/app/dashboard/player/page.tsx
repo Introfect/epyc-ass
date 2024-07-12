bd8ffaab2f15206fcdec568644d2e64c704feeb5
Aayushman Pratap <76990626+Introfect@users.noreply.github.com>
2024-07-12 22:35:27 +0530

    naming changed

--- src/app/dashboard/player/page.tsx
+++ src/app/dashboard/player/page.tsx
@@ -4,7 +4,7 @@ import useFetch from '@/lib/MatchData'
 
 type Props = {}
 
-export default async function page({}: Props) {
+export default async function Page({}: Props) {
   const {fetchCsvData}=useFetch()
   const playerData= await fetchCsvData('https://epyc-ass.vercel.app/cricket_data.csv')
   return (
